4efd15481d9536d221442adeb24fea7bbed5979f
MichaelAtsma <michaelatsma@msn.com>
2025-08-20 11:26:26 +0200

    Change displayed text based on correct or not

--- exercises/Asking questions in Dodona/evaluation/oracle_test.py
+++ exercises/Asking questions in Dodona/evaluation/oracle_test.py
@@ -6,13 +6,18 @@ from evaluation_utils import EvaluationResult, Message
 # - de overige argumenten zijn die uit het testplan
 #   (de getallen 5 en 6 in dit geval)
 def evaluate_test(context, secret_number):
+    correct = secret_number == context.actual
+    if correct:
+        display_text = "Goed zo! Je hebt het geheime getal gevonden."
+    else:
+        display_text = "De waarde van het geheime getal kan je enkel krijgen door een vraag te stellen via Dodona."
     return EvaluationResult(
       # Boolean of dat het resultaat juist is
-      result = secret_number == context.actual,
+      result = correct,
       # De "verwachte waarde" om te tonen op Dodona
-      dsl_expected = repr("De waarde van het geheime getal kan je enkel krijgen door een vraag te stellen via Dodona."),
+      dsl_expected = repr(display_text),
       # De eigenlijke waarde uit de oplossing om te tonen op Dodona
       dsl_actual = repr(context.actual),
       # Optionale lijst van berichten om te tonen op Dodona
-      messages = []
+      messages = [Message("Test")]
     )
