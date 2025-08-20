f2164252f9751a6a01f7c69b0700cc29004da9d1
MichaelAtsma <michaelatsma@msn.com>
2025-08-20 19:11:05 +0200

    Fix division operation and update range limit so the file doesn't change when I want a different limit

--- exercises/Collatz Conjecture -io -multiLineO/solution/other/checking_sequence_length.py
+++ exercises/Collatz Conjecture -io -multiLineO/solution/other/checking_sequence_length.py
@@ -5,7 +5,7 @@ def Collatz(n, lim=1000):
         if x == 1:
             return i
         elif x % 2 == 0:
-            x = x/2
+            x = x//2
         else:
             x = 3*x+1
         i += 1
@@ -13,7 +13,8 @@ def Collatz(n, lim=1000):
 
 hoogste_i = 0
 hoogste_n = 1
-for n in range(1,26):
+lim = int(input("Geef een limiet: "))
+for n in range(1, lim+1):
     i = Collatz(n)
     if i > 1000:
         print(f"{n} bereikt 1 niet binnen 1000 stappen.")
