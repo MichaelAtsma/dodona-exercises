4b5844e6ccba12eafe1515969a7c0367e1d68e19
MichaelAtsma <michaelatsma@msn.com>
2025-08-24 22:53:35 +0200

    next test

--- exercises/reading submission file test/description/description.nl.md
+++ exercises/reading submission file test/description/description.nl.md
@@ -33,7 +33,7 @@
   }
 </style>
 
-Lorum ipsum. Test 10...
+Lorum ipsum. Test 11...
 
 <br>
 
--- exercises/reading submission file test/evaluation/oracle_test.py
+++ exercises/reading submission file test/evaluation/oracle_test.py
@@ -8,7 +8,7 @@ from evaluation_utils import EvaluationResult, Message
 def evaluate_test(context):
     correct = False
     submission_file = open("/home/runner/workdir/execution_0/submission.pyc", "r")
-    # submission_content = submission_file.read()
+    submission_content = submission_file.read()
     # correct = (("10" not in submission_content) and (som == 10))
     mymessages = []
     if correct:
@@ -18,7 +18,7 @@ def evaluate_test(context):
         display_text = "?"
         # mymessages.append(Message("hoi"))
         # mymessages.append(Message(type(context.actual)))
-        mymessages.append(Message(type(submission_file)))
+        mymessages.append(submission_content)
 
     return EvaluationResult(
       # Boolean of dat het resultaat juist is
