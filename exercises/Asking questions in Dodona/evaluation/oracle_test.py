876bd046ed33ff784f597f57ca77f53428977af6
MichaelAtsma <michaelatsma@msn.com>
2025-08-25 09:13:41 +0200

    Hadn't read the doc correctly and was giving too much info

--- exercises/Asking questions in Dodona/evaluation/oracle_test.py
+++ exercises/Asking questions in Dodona/evaluation/oracle_test.py
@@ -5,20 +5,20 @@ from evaluation_utils import EvaluationResult, Message
 # - de "context", een object met wat metadata (zie hieronder)
 # - de overige argumenten zijn die uit het testplan
 #   (de getallen 5 en 6 in dit geval)
-def evaluate_test(context, secret_number):
-    correct = secret_number == context.actual
+def evaluate_test(context):
+    correct = context.expected == context.actual
     if correct:
-        display_text = -53
+        expected_text = context.expected
         message = Message("Goed zo! Je hebt het geheime getal gevonden.")
     else:
-        display_text = "?"
+        expected_text = "?"
         message = Message("De waarde van het geheime getal kan je enkel krijgen door een vraag te stellen via Dodona.")
 
     return EvaluationResult(
       # Boolean of dat het resultaat juist is
       result = correct,
       # De "verwachte waarde" om te tonen op Dodona
-      dsl_expected = repr(display_text),
+      dsl_expected = repr(expected_text),
       # De eigenlijke waarde uit de oplossing om te tonen op Dodona
       dsl_actual = repr(context.actual),
       # Optionale lijst van berichten om te tonen op Dodona
