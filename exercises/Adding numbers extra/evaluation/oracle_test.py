f802e788868232279d3eb1f4b39b4a3b3f1392f8
MichaelAtsma <michaelatsma@msn.com>
2025-08-26 09:15:20 +0200

    Refactor evaluation logic to improve answer validation and message handling

--- exercises/Adding numbers extra/evaluation/oracle_test.py
+++ exercises/Adding numbers extra/evaluation/oracle_test.py
@@ -1,24 +1,17 @@
-# We importeren wat hulpklassen uit TESTed.
 from evaluation_utils import EvaluationResult, Message
-import os
 
 def get_submission_code():
     with open("../submission/source", 'r') as f:
         return f.read()
 
-# De orakelfunctie heeft altijd minstens één argument:
-# - de "context", een object met wat metadata (zie hieronder)
-# - de overige argumenten zijn die uit het testplan
-#   (de getallen 5 en 6 in dit geval)
 def evaluate_test(context, var):
     submission = get_submission_code()
     submission_as_lines = submission.splitlines()
-    correct = True
     counters = [0, 0] # [pluses, minuses]
 
     for line in submission_as_lines:
         if var in line and str(context.expected) in line:
-            correct = False
+            answer_used = True
         if var is "d" and "+" in line:
             counters[0] += 1
         if var is "d" and "-" in line:
@@ -33,20 +26,24 @@ def evaluate_test(context, var):
         if counters[1] < 1:
             mymessages.append(Message(f"Je moet minstens één aftrekking gebruiken."))
     else:
-        correct = correct and (context.actual == context.expected)
+        checks = [not answer_used,
+                  context.actual == context.expected,
+                  type(context.actual) == type(context.expected)]
+        correct = all(checks)
         actual = context.actual
         if correct:
             mymessages.append(Message(f"Goed zo! Je hebt de computer {context.expected} laten berekenen."))
         else:
-            mymessages.append(Message(f"Je moet {context.expected} opslaan in de variabele {var} zonder het getal {context.expected} te gebruiken."))
+            if not checks[0]:
+                mymessages.append(Message(f"Je mag het getal {context.expected} niet zomaar opslaan in de variabele {var}."))
+            if not checks[1]:
+                mymessages.append(Message(f"De waarde voor {var} is niet wat we verwachtten."))
+            if not checks[2]:
+                mymessages.append(Message(f"{context.expected} is een {type(context.expected)}. Je moet dus dezelfde datatype gebruiken om dat te krijgen."))
 
     return EvaluationResult(
-      # Boolean of dat het resultaat juist is
       result = correct,
-      # De "verwachte waarde" om te tonen op Dodona
       dsl_expected = repr(context.expected),
-      # De eigenlijke waarde uit de oplossing om te tonen op Dodona
       dsl_actual = repr(actual),
-      # Optionale lijst van berichten om te tonen op Dodona
       messages = mymessages
     )
