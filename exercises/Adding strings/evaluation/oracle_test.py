0028445952082cd7032aa904b66aca9a823ded41
MichaelAtsma <michaelatsma@msn.com>
2025-08-26 10:47:43 +0200

    Refactor messages in evaluate_test to use repr for expected values

--- exercises/Adding strings/evaluation/oracle_test.py
+++ exercises/Adding strings/evaluation/oracle_test.py
@@ -14,14 +14,14 @@ def evaluate_test(context):
     
     mymessages = []
     if correct:
-        mymessages.append(Message(f"Goed zo! Je hebt de computer {context.expected} laten berekenen."))
+        mymessages.append(Message(f"Goed zo! Je hebt de computer {repr(context.expected)} laten berekenen."))
     else:
         if not checks[0]:
-            mymessages.append(Message(f"Je mag de zin {context.expected} niet gebruiken in je code."))
+            mymessages.append(Message(f"Je mag de zin {repr(context.expected)} niet gebruiken in je code."))
         if not checks[1]:
             mymessages.append(Message("Je moet een optelling gebruiken."))
         if not checks[2]:
-            mymessages.append(Message(f"{context.expected} is een tekst (string). Je moet dus tekst gebruiken om dat te krijgen."))
+            mymessages.append(Message(f"{repr(context.expected)} is een tekst (string). Je moet dus tekst gebruiken om dat te krijgen."))
         if not checks[3]:
             mymessages.append(Message("De uitkomst is niet wat we verwachtten."))
 
