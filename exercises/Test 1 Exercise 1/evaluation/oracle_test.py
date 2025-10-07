5ca5b00bc5b4cc94f326909da158e60ffae82089
MichaelAtsma <michaelatsma@msn.com>
2025-10-07 11:51:18 +0200

    refactor evaluation functions to consolidate into a single evaluate_test function and fix correct expected value

--- exercises/Test 1 Exercise 1/evaluation/oracle_test.py
+++ exercises/Test 1 Exercise 1/evaluation/oracle_test.py
@@ -7,49 +7,17 @@ def get_submission_code():
 def lines_containing_var(text, var):
     return [line for line in text.splitlines() if var in line]
 
-def evaluate_test_int(context):
+def evaluate_test(context):
     correct = (type(context.actual) == type(context.expected)) and (context.actual == context.expected)
 
     mymessages = []
-    expected = "Omdat dit een toets is wordt het verwachte antwoord hier niet getoond."
-    if not correct:
-        if type(context.actual) == type(context.expected):
-            mymessages += [Message("Je hebt het juiste datatype gebruikt (integer), maar de waarde is incorrect.")]
+    if correct:
+        expected = context.expected
+    elif type(context.actual) == type(context.expected):
+            mymessages = [Message("Je hebt het juiste datatype gebruikt, maar de waarde is incorrect.")]
             expected = context.expected
-
-    return EvaluationResult(
-      result = correct,
-      dsl_expected = repr(expected),
-      dsl_actual = repr(context.actual),
-      messages = mymessages
-    )
-
-def evaluate_test_float(context):
-    correct = (type(context.actual) == type(context.expected)) and (context.actual == context.expected)
-
-    mymessages = []
-    expected = "Omdat dit een toets is wordt het verwachte antwoord hier niet getoond."
-    if not correct:
-        if type(context.actual) == type(context.expected):
-            mymessages += [Message("Je hebt het juiste datatype gebruikt (float), maar de waarde is incorrect.")]
-            expected = context.expected
-
-    return EvaluationResult(
-      result = correct,
-      dsl_expected = repr(expected),
-      dsl_actual = repr(context.actual),
-      messages = mymessages
-    )
-
-def evaluate_test_str(context):
-    correct = (type(context.actual) == type(context.expected)) and (context.actual == context.expected)
-
-    mymessages = []
-    expected = "Omdat dit een toets is wordt het verwachte antwoord hier niet getoond."
-    if not correct:
-        if type(context.actual) == type(context.expected):
-            mymessages += [Message("Je hebt het juiste datatype gebruikt (string), maar de waarde is incorrect.")]
-            expected = context.expected
+    else:
+        expected = "Omdat dit een toets is wordt het verwachte antwoord hier niet getoond."
 
     return EvaluationResult(
       result = correct,
