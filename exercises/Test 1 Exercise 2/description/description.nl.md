3d13e73d883e0fb2b4949c73bdca79000b5f5f42
MichaelAtsma <michaelatsma@msn.com>
2025-10-11 16:31:40 +0200

    bug fix: forgot to change a list to a dict after abstracting

---
+++ exercises/Test 1 Exercise 2/description/description.nl.md
@@ -0,0 +1,39 @@
+<script>
+  document.addEventListener("copy", function(e) {
+    e.preventDefault();
+    e.clipboardData.setData("text/plain", "");
+  });
+</script>
+
+<style>
+  .invisible-text {
+    color: transparent;
+    font-size: 0.1em;
+    display: inline;
+    margin: 0;
+    padding: 0;
+  }
+  /* To use this, put any text like this: 
+  <span class="invisible-text">Your invisible text here</span> 
+  */
+
+  table {
+    margin: 0 auto;       /* centers table horizontally */
+  }
+  th {
+    font-size: 1.2em !important;
+    white-space: nowrap;
+  }
+  td {
+    white-space: nowrap;
+  }
+</style>
+
+# <b>Opdracht</b>
+1. Maak een variabele <code>getal1</code> die de waarde <code>5</code> opslaat.
+2. Maak een variabele <code>getal2</code> die de waarde <code>-9</code> opslaat.
+3. Maak een variabele <code>woord</code> die het woord <code>octopus</code> opslaat.
+4. Maak een variabele <code>som</code> die de som van <code>getal1</code> en <code>getal2</code> opslaat.
+5. Maak een variabele <code>product</code> die het product van <code>getal1</code> en <code>getal2</code> opslaat.
+6. Maak een variabele <code>meerdere_woorden</code> die <code>getal1</code> keer <code>woord</code> opslaat.
+7. 
---
+++ exercises/Test 1 Exercise 2/evaluation/oracle_test.py
@@ -0,0 +1,30 @@
+from evaluation_utils import EvaluationResult, Message # type: ignore
+
+def get_submission_code():
+    with open("../submission/source", 'r') as f:
+        return f.read()
+
+def lines_containing_var(text, var):
+    return [line for line in text.splitlines() if var in line]
+
+def evaluate_test(context, show_no_message_for_these_values=[], show_no_message_if_this_character_in_value=[]):
+    correct = (type(context.actual) == type(context.expected)) and (context.actual == context.expected)
+
+    mymessages = []
+    if correct:
+        expected = context.expected
+    elif all([type(context.actual) == type(context.expected), 
+              context.actual not in show_no_message_for_these_values, 
+              not any(char in str(context.actual) for char in show_no_message_if_this_character_in_value)]):
+            mymessages = [Message("Je hebt het juiste datatype gebruikt, maar de waarde is incorrect.")]
+            expected = context.expected
+    else:
+        expected = ""
+        mymessages = [Message("Omdat dit een toets is wordt het verwachte antwoord hier niet getoond.")]
+
+    return EvaluationResult(
+      result = correct,
+      dsl_expected = repr(expected),
+      dsl_actual = repr(context.actual),
+      messages = mymessages
+    )
