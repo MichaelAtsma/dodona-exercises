45348a662acc06e6359d835b7c75e53cf0cf647d
MichaelAtsma <michaelatsma@msn.com>
2025-10-23 00:26:47 +0200

    Copilot thought . and ! were the same apparently lol, and I didn't bother to check

--- exercises/Test 1 Exercise 4/solution/oplossing.py
+++ exercises/Test 1 Exercise 4/solution/oplossing.py
@@ -1,2 +1,2 @@
 score = 6.66667
-Felicitatie = f"Als je de oefeningen tot en met deze correct hebt, dan heb je al een {score:.3f}/10 behaald voor je toets."
+Felicitatie = f"Als je de oefeningen tot en met deze correct hebt, dan heb je al een {score:.3f}/10 behaald voor je toets!"
