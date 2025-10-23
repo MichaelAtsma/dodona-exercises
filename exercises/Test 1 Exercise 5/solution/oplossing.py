ff1ab216f9ddeac31bf049b6f9dffcfbcbb0c10e
MichaelAtsma <michaelatsma@msn.com>
2025-10-23 06:17:35 +0200

    reformat solution to use 4 spaces instead of a tab

--- exercises/Test 1 Exercise 5/solution/oplossing.py
+++ exercises/Test 1 Exercise 5/solution/oplossing.py
@@ -1,5 +1,5 @@
 def ToetsPunt(punten, max_punten, max_score):
-	score = punten * max_score / max_punten
-	# of: score = (punten / max_punten) * max_score
-	# De tests worden enkel gedaan met die getallen waarbij deze twee uitdrukkingen gelijk zijn.
-	return score
+    score = punten * max_score / max_punten
+    # of: score = (punten / max_punten) * max_score
+    # De tests worden enkel gedaan met die getallen waarbij deze twee uitdrukkingen gelijk zijn.
+    return score
