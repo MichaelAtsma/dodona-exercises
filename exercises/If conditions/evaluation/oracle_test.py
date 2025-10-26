6c446ec92452e8c91c7f2bdc51c6078c050e33d4
MichaelAtsma <michaelatsma@msn.com>
2025-10-26 17:38:50 +0100

    Add feedback message for logical operators not used

--- exercises/If conditions/evaluation/oracle_test.py
+++ exercises/If conditions/evaluation/oracle_test.py
@@ -8,30 +8,21 @@ def get_submission_code():
 def lines_containing_var(text, var):
     return [line for line in text.splitlines() if (f"{var}=" in line or f"{var} =" in line)]
 
-def evaluate_test(context, match_regex, mandatory_logical_operators_and_descriptions, correct_message_template, wrong_value_message_template):
+def evaluate_test(context, match_regex, correct_message_template, wrong_value_message_template):
     submission = get_submission_code()
     checks = {}
     checks["correct value"] = context.actual == context.expected
 
     m = re.fullmatch(match_regex, submission)
-    condition = m.group(1) if m else ""
     checks["code matches regex"] = m is not None
-
-    checks["mandatory logical operators used"] = True
-    for operator in mandatory_logical_operators_and_descriptions.keys():
-        if operator not in condition:
-            checks["mandatory logical operators used"] = False
 
     correct = all(checks.values())
     mymessages = []
     if correct:
-        mymessages.append(Message(correct_message_template.format(condition)))
+        mymessages.append(Message(correct_message_template.format(m.group(1))))
     else:
         if not checks["correct value"]:
-            mymessages.append(Message(wrong_value_message_template.format(condition)))
-        if not checks["mandatory logical operators used"]:
-            missing_operators = [desc for op, desc in mandatory_logical_operators_and_descriptions.items() if op not in condition]
-            mymessages.append(Message(f"Je moet in je voorwaarde gebruikmaken van: {', '.join(missing_operators)}."))
+            mymessages.append(Message(wrong_value_message_template.format(m.group(1))))
         if not checks["code matches regex"]:
             mymessages.append(Message(f"Je mag enkel de voorwaarde aanpassen. Zorg ervoor dat je de rest van de code niet wijzigt."))
         
