5b742e1abdeba228d77fe33bb764ce264e5a49a4
MichaelAtsma <michaelatsma@msn.com>
2026-01-02 10:33:35 +0100

    init commit

---
+++ exercises/Combining functions and if-elif-else exercise 17/evaluation/generate_tests.py
@@ -0,0 +1,32 @@
+import pyperclip
+import itertools
+import random
+
+def copy_to_clipboard(text):
+    pyperclip.copy(text)
+
+def FilmDuur(uren, minuten):
+    if uren >= 2:
+        conclusie = "Lange film."
+    elif uren == 0:
+        conclusie = "Hele korte film."
+    elif minuten <= 20:
+        conclusie = "Korte film."
+    else:
+        conclusie = "Normale film."
+    
+    return conclusie
+
+X = [(0, 45), (1, 0), (1, 10), (1, 20), (1, 30), (2, 0), (2, 20)]
+Uren = range(0, 2+1)
+Minuten = range(0, 59+1)
+X = itertools.product(Uren, Minuten)
+
+result = ""
+for args in X:
+    result += f">>> FilmDuur({', '.join(map(repr, args))})\n"
+    result += f"{repr(FilmDuur(*args))}\n"
+
+copy_to_clipboard(result.strip())
+print("Copied to clipboard:")
+print(result)
---
+++ exercises/Combining functions and if-elif-else exercise 17/solution/oplossing.py
@@ -0,0 +1,11 @@
+def FilmDuur(uren, minuten):
+    if uren >= 2:
+        conclusie = "Lange film."
+    elif uren == 0:
+        conclusie = "Hele korte film."
+    elif minuten <= 20:
+        conclusie = "Korte film."
+    else:
+        conclusie = "Normale film."
+    
+    return conclusie
