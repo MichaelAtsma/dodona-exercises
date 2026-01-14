2f9385ff474c129224bf434ef11a7b403c982954
MichaelAtsma <michaelatsma@msn.com>
2026-01-14 21:25:58 +0100

    bug fix: <= was inconsistent with description

--- exercises/Test 2 Exercise 4/evaluation/generate_tests.py
+++ exercises/Test 2 Exercise 4/evaluation/generate_tests.py
@@ -9,7 +9,7 @@ def InkomstenBelasting(inkomen):
     if inkomen < 10000:
         belasting = inkomen * 0.10
         bericht = f"Je moet 10% belasting betalen. Dat is dus €{belasting}."
-    elif inkomen <= 50000:
+    elif inkomen < 50000:
         belasting = inkomen * 0.20
         bericht = f"Je moet 20% belasting betalen. Dat is dus €{belasting}."
     else:
@@ -20,7 +20,7 @@ def InkomstenBelasting(inkomen):
 def InkomstenBelastingHelper(inkomen):
     if inkomen < 10000:
         return inkomen * 0.10
-    elif inkomen <= 50000:
+    elif inkomen < 50000:
         return inkomen * 0.20
     else:
         return inkomen * 0.40
--- exercises/Test 2 Exercise 4/solution/oplossing.py
+++ exercises/Test 2 Exercise 4/solution/oplossing.py
@@ -2,7 +2,7 @@ def InkomstenBelasting(inkomen):
     if inkomen < 10000:
         belasting = inkomen * 0.10
         bericht = f"Je moet 10% belasting betalen. Dat is dus €{belasting}."
-    elif inkomen <= 50000:
+    elif inkomen < 50000:
         belasting = inkomen * 0.20
         bericht = f"Je moet 20% belasting betalen. Dat is dus €{belasting}."
     else:
