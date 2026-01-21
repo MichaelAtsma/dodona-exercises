485a5a80f3902fa03dc09d412404d2d0285a1c52
MichaelAtsma <michaelatsma@msn.com>
2026-01-21 18:57:23 +0100

    can I do 3 dots to go up another directory?

--- exercises/Testing/evaluation/oracle_test.py
+++ exercises/Testing/evaluation/oracle_test.py
@@ -7,12 +7,12 @@ def get_submission_code():
         return f.read()
 
 def get_boilerplate():
-    return list(os.walk("../submission/"))
+    return list(os.walk(".../submission/"))
 
 # The directory structure is as follows:
 # ../:
 #   - submission/
-#     - source/
+#     - source    (the student's code)
 #   - resources/
 #     - suite.yaml
 #     - oracle_test.py
