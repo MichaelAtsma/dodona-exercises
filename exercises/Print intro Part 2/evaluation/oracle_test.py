bffbc7483360b6cfb4c516a449bbc454f36bffad
MichaelAtsma <michaelatsma@msn.com>
2026-01-21 20:13:55 +0100

    testing

--- exercises/Print intro Part 2/evaluation/oracle_test.py
+++ exercises/Print intro Part 2/evaluation/oracle_test.py
@@ -37,7 +37,7 @@ def evaluate_test(context, match_regex, mandatory_logical_operators_and_descript
         
     return EvaluationResult(
       result = False,
-      dsl_expected = context.expected,
+      dsl_expected = repr(context.expected),
       dsl_actual = repr(context.actual),
       messages = mymessages
     )
