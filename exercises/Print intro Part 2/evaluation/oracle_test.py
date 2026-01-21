484f4e59f9e87712cb24440f7d94092dc5944e97
MichaelAtsma <michaelatsma@msn.com>
2026-01-21 20:11:00 +0100

    testing if repr is needed, it shouldn't be though...

--- exercises/Print intro Part 2/evaluation/oracle_test.py
+++ exercises/Print intro Part 2/evaluation/oracle_test.py
@@ -37,7 +37,7 @@ def evaluate_test(context, match_regex, mandatory_logical_operators_and_descript
         
     return EvaluationResult(
       result = correct,
-      dsl_expected = context.expected,
-      dsl_actual = context.actual,
+      dsl_expected = repr(context.expected),
+      dsl_actual = repr(context.actual),
       messages = mymessages
     )
