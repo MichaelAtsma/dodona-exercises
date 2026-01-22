d7490c6f5c29c49a580f5efe5fec175a53d0edf9
MichaelAtsma <michaelatsma@msn.com>
2026-01-22 05:26:49 +0100

    displaying expected and actual as messages now because how is this supposed to work?

--- exercises/For-loop intro Part 1/evaluation/oracle_test.py
+++ exercises/For-loop intro Part 1/evaluation/oracle_test.py
@@ -52,10 +52,18 @@ def evaluate_test(context, match_regex, mandatory_texts_and_descriptions, forbid
             mymessages.append(Message(f"Je mag enkel iets tussen de haakjes schrijven. Zorg ervoor dat je de rest van de code niet wijzigt."))
         if not checks["student contribution type is not string"]:
             mymessages.append(Message(f"Je hebt een getal nodig in de range-functie, geen tekst."))
-        
+    
+    mymessages.append(Message(f"Je code gaf de volgende uitkomst:"))
+    for line in context.actual.splitlines():
+        mymessages.append(Message(line))
+
+    mymessages.append(Message(f"Verwachte uitkomst:"))
+    for line in context.expected.splitlines():
+        mymessages.append(Message(line))
+
     return EvaluationResult(
       result = correct,
-      dsl_expected = context.expected.splitlines(),
-      dsl_actual = context.actual.splitlines(),
+      dsl_expected = "",
+      dsl_actual = "",
       messages = mymessages
     )
