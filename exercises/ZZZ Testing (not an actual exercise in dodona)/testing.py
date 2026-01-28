807a7f921f544692144a009d3ff901f6e5878d91
MichaelAtsma <michaelatsma@msn.com>
2026-01-28 19:08:44 +0100

    testing

--- exercises/ZZZ Testing (not an actual exercise in dodona)/testing.py
+++ exercises/ZZZ Testing (not an actual exercise in dodona)/testing.py
@@ -1,5 +1,15 @@
-import random
-import pycountry
+import pyperclip
 
-country = random.choice(list(pycountry.countries)).name
-print(country)
+def copy_to_clipboard(text):
+    pyperclip.copy(text)
+
+result = ""
+for i in range(51):
+    if i % 10 == 4:
+        result += f"print('{i} eindigt op een 4')\n"
+    else:
+        result += f"print('{i} eindigt niet op een 4')\n"
+
+copy_to_clipboard(result)
+print("Copied to clipboard:")
+print(result)
