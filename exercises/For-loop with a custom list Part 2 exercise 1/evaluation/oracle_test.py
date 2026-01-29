f3fc467839c8d5718059c55ffb4508b920972e65
MichaelAtsma <michaelatsma@msn.com>
2026-01-29 01:26:23 +0100

    init commit

---
+++ exercises/For-loop with a custom list Part 2 exercise 1/evaluation/oracle_test.py
@@ -0,0 +1,62 @@
+from evaluation_utils import EvaluationResult, Message # type: ignore
+import re
+
+def get_submission_code():
+    with open("../submission/source", 'r') as f:
+        return f.read()
+
+def lines_containing_var(text, var):
+    return [line for line in text.splitlines() if (f"{var}=" in line or f"{var} =" in line)]
+
+def evaluate_test(context, match_regex, list_variable_name, mandatory_texts_and_descriptions, forbidden_texts_and_descriptions, correct_message_template, wrong_value_message_template):
+    submission = get_submission_code()
+    checks = {}
+    checks["correct value"] = context.actual == context.expected
+
+    m = re.fullmatch(match_regex, submission)
+    student_contribution = m.group(1) if m else ""
+    checks["code matches regex"] = m is not None
+
+    checks["list variable name used correctly"] = m.group(3) == list_variable_name
+
+    missing_texts_and_descriptions = {}
+    checks["mandatory texts used"] = True
+    for text in mandatory_texts_and_descriptions.keys():
+        if text not in student_contribution:
+            checks["mandatory texts used"] = False
+            missing_texts_and_descriptions[text] = mandatory_texts_and_descriptions[text]
+
+    used_forbidden_texts_and_descriptions = {}
+    checks["forbidden text not used"] = True
+    for forbidden_text in forbidden_texts_and_descriptions.keys():
+        if forbidden_text in student_contribution:
+            checks["forbidden text not used"] = False
+            used_forbidden_texts_and_descriptions[forbidden_text] = forbidden_texts_and_descriptions[forbidden_text]
+
+    checks["student contribution type does not contain strings"] = '"' not in student_contribution and "'" not in student_contribution
+
+    correct = all(checks.values())
+    mymessages = []
+    if correct:
+        mymessages.append(Message(correct_message_template))
+    else:
+        if not checks["correct value"]:
+            mymessages.append(Message(wrong_value_message_template))
+        if not checks["mandatory texts used"]:
+            mymessages.append(Message(f"Je moet gebruik maken van: {', '.join(set(missing_texts_and_descriptions.values()))}."))
+        if not checks["forbidden text not used"]:
+            mymessages.append(Message(f"Je mag geen gebruik maken van: {', '.join(set(used_forbidden_texts_and_descriptions.values()))}."))
+        if not checks["code matches regex"]:
+            mymessages.append(Message(f"Je mag enkel de underscores vervangen. Zorg ervoor dat je de rest van de code niet wijzigt."))
+        if not checks["list variable name used correctly"]:
+            mymessages.append(Message(f"Zorg ervoor dat je in de for-lus de juiste naam voor de lijst gebruikt."))
+        if not checks["student contribution type does not contain strings"]:
+            mymessages.append(Message(f"Hoewel het antwoord misschien juist is, moet je getallen printen. Dan heb je dus geen aanhalingstekens nodig."))
+        
+
+    return EvaluationResult(
+      result = correct,
+      readable_expected = context.expected,
+      readable_actual = context.actual,
+      messages = mymessages
+    )
---
+++ exercises/For-loop with a custom list Part 2 exercise 1/solution/oplossing.py
@@ -0,0 +1,4 @@
+hardloop_afstanden = [5, 10, 15, 20, 21.095, 42.195]
+          
+for afstand in hardloop_afstanden:
+    print(afstand)
