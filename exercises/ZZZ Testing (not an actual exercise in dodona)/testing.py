b9db38e736f0e049e28176595fd3017e15f91059
MichaelAtsma <michaelatsma@msn.com>
2026-02-02 23:41:32 +0100

    refactor the regex match so that it doesn't run into an exponential loop with fullmatch checking all backtraces

--- exercises/ZZZ Testing (not an actual exercise in dodona)/testing.py
+++ exercises/ZZZ Testing (not an actual exercise in dodona)/testing.py
@@ -1,15 +1,38 @@
-import pyperclip
+submission = 'for i in range (111):\n    if i < 50:\n        print (f"{i}% is een onvoldoende.")\n    elif i <= 100:\n        print (f"{i}% is een voldoende.")   \n    else:\n        print(f"Je kan geen {i}% scoren.")\n\n'
+submission = r"""for i in range (111):
+    if i < 50:
+        print (f"{i}% is een onvoldoende.")
+    elif i <= 100:
+        print (f"{i}% is een voldoende.")
 
-def copy_to_clipboard(text):
-    pyperclip.copy(text)
-
-result = ""
-for i in range(51):
-    if i % 10 == 4:
-        result += f"print('{i} eindigt op een 4.')\n"
     else:
-        result += f"print('{i} eindigt niet op een 4.')\n"
+        print(f"Je kan geen {i}% scoren.")
 
-copy_to_clipboard(result)
-print("Copied to clipboard:")
-print(result)
+
+
+
+"""
+
+regmatch = "[\\n \\t]*for [a-zA-Z_]+[a-zA-Z0-9_]* in range[ ]*\\(.+\\):((?:\\n(?:[ \\t]+.*)?)*)\\n*"
+
+import re
+
+m1 = re.match(regmatch, submission)
+if m1:
+    print("re.match MATCHED")
+    print(m1.group(0))
+    print("-----")
+    print(m1.group(1))
+    print("=====")
+else:
+    print("re.match DID NOT MATCH")
+
+m2 = re.fullmatch(regmatch, submission)
+if m2:
+    print("re.fullmatch MATCHED")
+    print(m2.group(0))
+    print("-----")
+    print(m2.group(1))
+    print("=====")
+else:
+    print("re.fullmatch DID NOT MATCH")
