eedbbaa8dca069e94d5d4471f331f2107a5e2876
MichaelAtsma <michaelatsma@msn.com>
2026-02-05 04:02:35 +0100

    init commit

---
+++ exercises/For-loop in a function with a given list Part 1 exercise 2/evaluation/generate_tests.py
@@ -0,0 +1,35 @@
+import random
+import pyperclip
+import itertools
+import os
+
+def copy_to_clipboard(text):
+    pyperclip.copy(text)
+
+def Dubbels(getallen):
+    return "\n".join([f"Het dubbele van {getal} is {2 * getal}" for getal in getallen])
+
+function_effect = "prints"
+function = Dubbels
+bulk_test = True
+
+if not bulk_test:
+    X = [([5, 3, -7, 42, -91, 28],),
+         ([1, 2, 4, 8, 16, 32],),
+         ([5, 4, 3, 2, 1, 0],)]
+else:
+    X = [([random.randint(-100, 100) for _ in range(10)],) for _ in range(100)]
+
+
+
+result = ""
+for args in X:
+    result += f">>> {function.__name__}({', '.join(map(str, args))})\n"
+    if function_effect == "returns":
+        result += f"{repr(function(*args))}\n"
+    else:
+        result += f"{function(*args)}\n"
+
+copy_to_clipboard(result.strip())
+print("Copied to clipboard:")
+print(result)
---
+++ exercises/For-loop in a function with a given list Part 1 exercise 2/solution/oplossing.py
@@ -0,0 +1,3 @@
+def Dubbels(getallen):
+    for getal in getallen:
+        print(f"Het dubbele van {getal} is {2 * getal}")
