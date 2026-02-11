80bb09e1601a39128859811ac70f2e7d3699211f
MichaelAtsma <michaelatsma@msn.com>
2026-02-12 00:38:32 +0100

    bug fix: regex

--- exercises/ZZZ Testing (not an actual exercise in dodona)/testing.py
+++ exercises/ZZZ Testing (not an actual exercise in dodona)/testing.py
@@ -1,21 +1,19 @@
-import io
-import sys
+import re
 
-def capture_output(func, *args, **kwargs):
-    old_stdout = sys.stdout
-    sys.stdout = io.StringIO()
-    try:
-        return_value = func(*args, **kwargs)
-        return return_value, sys.stdout.getvalue()
-    finally:
-        sys.stdout = old_stdout
+regex_tomatch = "[\\n \\t]*([a-zA-Z_]+[a-zA-Z0-9_]*)[ ]*=[ ]*\\[[ ]*10\\.99[ ]*,[ ]*5\\.49[ ]*,[ ]*3\\.50[ ]*\\][ \\t]*[\\n]+[\\n \\t]*totaal[ ]*=[ ]*0[\\n \\t]*[\\n]+for[ ]+([a-zA-Z_]+[a-zA-Z0-9_]*)[ ]+in[ ]+\\1[ ]*:[ ]*[\\n \\t]*[ \\t]+(totaal[ ]*(=[ ]*totaal[ ]*\\+|\\+=)[ ]*\\2)[\\n \\t]*[\\n]+print[ ]*\\([ ]*totaal[ ]*\\)[\\n \\t]*"
+submission = """prijzen = [10.99, 5.49, 3.50]
 
-def PrintDeelbaarheden3(getallen):
-    for getal in getallen:
-        if getal % 3 == 0:
-            print(f"{getal} is deelbaar door 3.")
-        else:
-            print(f"{getal} is niet deelbaar door 3.")
+totaal = 0
+for prijs in prijzen:
+    totaal = totaal + prijs
 
-_, theoutput = capture_output(PrintDeelbaarheden3, [1, 2, 3, 4, 5, 6])
-print(f"-----LOOK HERE--------{theoutput}------I CAUGHT IT-------")
+print(totaal)"""
+
+m = re.fullmatch(regex_tomatch, submission)
+if m:
+    print(m.group(0))
+    print(m.group(1))
+    print(m.group(2))
+    print(m.group(3))
+else:
+    print("No match")
