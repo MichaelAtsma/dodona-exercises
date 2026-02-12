4ebe4cf84a1126cd47684b929c6ce0a1ae2f73b5
MichaelAtsma <michaelatsma@msn.com>
2026-02-12 05:22:14 +0100

    init commit

---
+++ exercises/Keeping a subtotal in a for-loop exercise 1/solution/oplossing.py
@@ -0,0 +1,7 @@
+prijzen = [10.99, 5.49, 3.50, 9.99, 2.50]
+
+geld_over_in_portemonnee = 70
+for prijs in prijzen:
+    geld_over_in_portemonnee = geld_over_in_portemonnee - prijs
+
+print(geld_over_in_portemonnee)
