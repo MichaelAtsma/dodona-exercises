47ac2320aca0b3168234d8b1974330fb23d4a962
MichaelAtsma <michaelatsma@msn.com>
2026-02-12 09:18:24 +0100

    consistency fix: match evaluation with description (as was the plan)

--- exercises/For-loop in a function with a given list Part 2 exercise 5/solution/oplossing.py
+++ exercises/For-loop in a function with a given list Part 2 exercise 5/solution/oplossing.py
@@ -1,9 +1,9 @@
 def PrintWachtwoordLengtes(wachtwoorden):
     for wachtwoord in wachtwoorden:
         lengte = len(wachtwoord)
-        if lengte == 8:
+        if lengte == 10:
             print(f"{wachtwoord} is precies lang genoeg als wachtwoord.")
-        elif lengte > 8:
+        elif lengte > 10:
             print(f"{wachtwoord} is een lang wachtwoord.")
         else:
             print(f"{wachtwoord} is een te kort wachtwoord.")
