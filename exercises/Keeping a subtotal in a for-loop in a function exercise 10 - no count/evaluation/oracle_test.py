2cbd5bc4e70037b51c18f9d02eda7bb91c4e0616
MichaelAtsma <michaelatsma@msn.com>
2026-03-19 13:15:52 +0100

    debug testing

--- exercises/Keeping a subtotal in a for-loop in a function exercise 10 - no count/evaluation/oracle_test.py
+++ exercises/Keeping a subtotal in a for-loop in a function exercise 10 - no count/evaluation/oracle_test.py
@@ -24,7 +24,7 @@ def evaluate_test(context, forbidden_texts_and_descriptions):
     mymessages = []
     mymessages.append(Message(f"Jouw antwoord: {context.actual}"))
     mymessages.append(Message(f"Verwachte antwoord: {context.expected}"))
-    mymessages.append(Message(f"Checks: {checks}"))
+    # mymessages.append(Message(f"Checks: {checks}"))
     if not checks["forbidden text not used"]:
         mymessages.append(Message(f"Je mag geen gebruik maken van: {', '.join(used_forbidden_texts_and_descriptions.values())}."))
 
